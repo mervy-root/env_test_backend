cc9a40c07a92a9e7a15a8980e057a7470d2e5602
mervy-root <tsafackmerveille15@gmail.com>
2025-04-27 23:24:58 +0100

    phpmyadmin

---
+++ backend/phpmyadmin/config.inc.php
@@ -0,0 +1,26 @@
+<?php
+//Chiffrer les cookies de connexion dans phpmyadmin
+$cfg['blowfish_secret'] = 'motdepasse';
+
+$i = 0;
+
+$i++;
+
+//la methode d'authentification sera via cookie
+//l'utilisateur devra s'authentifier manuellement(login/mot de passe)
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+
+//Sécifie l'adresse du serveur mysql auquel phpMyAdmin va se connecter
+//Ici, db est le nom du conteneur docker (defini dans le docker-compose.yml)
+$cfg['Servers'][$i]['host'] = 'db';
+
+//compression des communication avec le serveur MySQL
+$cfg['Servers'][$i]['compress'] = false;
+
+//Permet aux utulisateurs de se connecter meme sans mot de passe
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+//durée de validite du cookie de connexion
+$cfg['LoginCookieValidity'] = 3600;
+
+//
